ff48facf43322b6f1b45c5fac9ebfd0d0261fe12
mclacore <mclacore@gmail.com>
2024-08-17 15:48:30 -0700

    formulating pw types

--- main.go
+++ main.go
@@ -3,9 +3,20 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"strings"
+	"time"
 )
 
+const (
+	lowercase = "abcdefghijklmnopqrstuvwxyz"
+	uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	numbers   = "0123456789"
+	special   = "!@#$%^&*()_+"
+)
+
+func generatePassword(n int) string {
+	pw := strings.Builder
+}
+
 func main() {
-	fmt.Println("Hello, World!")
-	fmt.Println("My favorite number is", rand.Intn(10))
 }
